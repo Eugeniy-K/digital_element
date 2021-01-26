4c2c3d0f4cbec130788c40e7cda0bda109786880
Eugeniy-K <eugeniykondr@gmail.com>
2021-01-26 14:16:38 +0500

    splash screen

--- lib/Screens/splash_screen.dart
+++ lib/Screens/splash_screen.dart
@@ -1,14 +1,48 @@
 import 'dart:async';
+import 'package:digital_element/main.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
 
+
+//начальный экран с заставкой
 class SplashScreen extends StatefulWidget {
   @override
   _SplashScreenState createState() => _SplashScreenState();
 }
 
 class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+    super.initState();
+    Timer(Duration(seconds: 2),
+        () => Navigator.of(context).pushReplacement(MaterialPageRoute(
+            builder: (BuildContext context) => MyHomePage(title: 'Flutter Demo Home Page'))));
+  }
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Container(
+        color: Color(0xFF1C6CD5,),
+        alignment: Alignment.center,
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Align(
+              alignment: Alignment(0.2, 0.0),
+              child:
+                SvgPicture.asset("assets/images/Vector.svg"),
+              ),
+            Padding(
+              padding: EdgeInsets.only(top: 10),
+              child: SvgPicture.asset("assets/images/Vector (1).svg"),),
+            Padding(
+              padding: EdgeInsets.only(top: 20),
+              child: SvgPicture.asset("assets/images/Vector (2).svg"),),
+            Padding(
+                padding: EdgeInsets.only(top: 10),
+                child: SvgPicture.asset("assets/images/Digital.svg"),)
+          ],
+        ),);
   }
 }
